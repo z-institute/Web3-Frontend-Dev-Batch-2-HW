df384c72e673b57ee8178636091f9fdef1fe016b
OwenLu <gg0125gg@gmail.com>
2023-10-31 22:10:23 +0800

    feat: check wallet balance

--- w1/Individual/coding/my-first-dapp/src/App.js
+++ w1/Individual/coding/my-first-dapp/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import logo from './logo.svg';
-import Web3Modal from "web3modal";
 import { useEffect } from 'react';
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 
 const web3Modal = new Web3Modal({
   network: "rinkeby", // or mainnet
@@ -13,7 +14,19 @@ function App() {
   useEffect(() => {
     const init = async () => {
       const instance = await web3Modal.connect();
+      const provider = new ethers.BrowserProvider(instance);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      const balance = await provider.getBalance(address);
+      // const ensAddress = await provider.lookupAddress(address);
+
       console.log(instance);
+      console.log(provider);
+      console.log(signer);
+      console.log(address);
+      console.log(balance);
+      console.log(ethers.formatEther(balance) + " ETH"); // this is big number
+      // console.log(ensAddress); // only available in mainnet
     };
     init();
   }, []);
