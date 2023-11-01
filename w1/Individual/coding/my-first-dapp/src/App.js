5b61afe6808f65bb2144c30721ec80358ef3e973
OwenLu <gg0125gg@gmail.com>
2023-11-01 18:16:38 +0800

    feat:interact with contract

--- w1/Individual/coding/my-first-dapp/src/App.js
+++ w1/Individual/coding/my-first-dapp/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import logo from './logo.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
 
@@ -10,6 +10,13 @@ const web3Modal = new Web3Modal({
 })
 
 function App() {
+  const [contract, setContract] = useState(null);
+  const [address, setAddress] = useState('0x0');
+  const [balance, setBalance] = useState('0');
+  const [ensAddress, setEnsAddress] = useState('0');
+  const [message, setMessage] = useState('');
+  const [paidMsg, setPaidMsg] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     const init = async () => {
@@ -20,15 +27,88 @@ function App() {
       const balance = await provider.getBalance(address);
       // const ensAddress = await provider.lookupAddress(address);
 
-      console.log(instance);
-      console.log(provider);
-      console.log(signer);
-      console.log(address);
-      console.log(balance);
-      console.log(ethers.formatEther(balance) + " ETH"); // this is big number
+      const contractAddr = '0xa3B688186379f69c28B3A3169F914a41eBA7dcFb';
+      const abi = [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "str",
+              "type": "string"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "str",
+              "type": "string"
+            }
+          ],
+          "name": "storePaidMsg",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "message",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "retrievePaidMsg",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ];
+      const contract = new ethers.Contract(contractAddr, abi, signer);
+
+      const msg = async () => {
+        const res = await contract.message();
+        setMessage(res);
+      };
+      await msg();
+
+      const paidmsg = async () => {
+        const res = await contract.retrievePaidMsg();
+        setPaidMsg(res);
+      };
+      await paidmsg();
+
+      // console.log(instance);
+      // console.log(provider);
+      // console.log(signer);
+      // console.log(address);
+      // console.log(balance);
+      // console.log(ethers.formatEther(balance) + " ETH"); // this is big number
       // console.log(ensAddress); // only available in mainnet
+      setAddress(address);
+      setBalance(ethers.formatEther(balance) + " ETH");
+      setContract(contract);
     };
     init();
+
   }, []);
 
   return (
@@ -36,18 +116,30 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {address}, your balance is :{balance}.<br />
+          your current free message is :{message}<br />
+          your current paid message is :{paidMsg}<br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        ></input>
+        <button onClick={async () => {
+          const tx = await contract.store(inputValue);
+          await tx.wait();
+          setMessage(await contract.message());
+        }}>free message</button>
+
+        <button onClick={async () => {
+          let payEtherAmount = ethers.parseEther("0.1");
+          let tx = await contract.storePaidMsg(
+            inputValue,
+            { value: payEtherAmount }
+          );
+          await tx.wait();
+          setPaidMsg(await contract.retrievePaidMsg());
+        }}>paid message</button>
       </header>
-    </div>
+    </div >
   );
 }
 
