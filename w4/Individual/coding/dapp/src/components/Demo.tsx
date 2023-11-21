3f02dae5ba2ab61fe2c500715c099a4df072cc3c
Elmer <s0227373691@gms.tku.edu.tw>
2023-11-22 06:12:01 +0800

    update code

--- w4/Individual/coding/dapp/src/components/Demo.tsx
+++ w4/Individual/coding/dapp/src/components/Demo.tsx
@@ -1,15 +1,17 @@
-import { useAccount, useContractRead } from "wagmi";
-import { Suspense, useState } from "react";
+import { useContractRead } from "wagmi";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
 import NFTabi from "./abi/basic.json";
 
 export default function Demo({ number, tokenID }: any) {
-  console.log(Number(number), Number(tokenID));
-
-  const [loading, setLoading] = useState(false);
-  const [image, setImage] = useState("");
   const [NFTData, setNFTData] = useState({} as any);
+  const image = useMemo(
+    () =>
+      NFTData.image &&
+      `https://ipfs.io/ipfs/${NFTData.image.replace("ipfs://", "")}`,
+    [NFTData]
+  );
 
   const add = NFTabi.address as `0x${string}`;
   const { data, isError, isLoading } = useContractRead({
@@ -20,40 +22,37 @@ export default function Demo({ number, tokenID }: any) {
   });
 
   const getNFTs = async (address?: string) => {
-    setLoading(true);
-    console.log(data);
     try {
       const res = await axios.get(
-        "https://bafybeiambgfilw53az2jo43udnleq76z6er2l56abp6xirgabwt4guqllu.ipfs.dweb.link/6792.json"
+        `https://ipfs.io/ipfs/${data.replace("ipfs://", "")}`
       );
-      setImage(res.data.image);
       setNFTData(res.data);
-      console.log(res.data);
     } catch (error) {
       console.error(error);
-      setLoading(false);
     }
   };
 
+  const openNFTImg = () => window.open(image);
+
+  useEffect(() => {
+    getNFTs();
+  }, []);
+
   return (
     <div className={`flex flex-col items-center justify-between p-24`}>
-      <a className="text-white">balance: {Number(number)}</a>
-      <a className="text-white">tokenID: {Number(tokenID)}</a>
-      <a className="text-white">tokenURI: {data as string}</a>
-      <div className="h-[394px] w-[394px]">
-        <img
-          src="https://ipfs.io/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/6962.png"
-          alt="NFT"
-          className="h-full w-full object-contain"
-        />
-      </div>
-
-      <button
-        onClick={() => getNFTs()}
-        className="p-10 bg-white text-black rounded-3xl hover:opacity-30 hover:text-white"
+      <div
+        className="h-[394px] w-[394px]"
+        style={{ background: "rgb(99, 99, 102)" }}
       >
-        getImage
-      </button>
+        <div style={{ cursor: "pointer" }} onClick={openNFTImg}>
+          <img src={image} alt="NFT" className="h-full w-full object-contain" />
+        </div>
+        <div className="p-2">
+          <p className="text-white">{NFTData.name}</p>
+          <p className="text-white">balance: {Number(number)}</p>
+          <p className="text-white">tokenID: {Number(tokenID)}</p>
+        </div>
+      </div>
     </div>
   );
 }
